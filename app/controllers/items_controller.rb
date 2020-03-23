74901e444842a94145b21141243237f31c9c2142
brandong1 <brandon@brandonleeproductions.com>
2020-03-23 13:08:20 -0600

    create item controller actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,25 @@
 class ItemsController < ApplicationController
+
+    def index
+        @items = Item.all
+        render json: @items, include: :category
+    end
+
+    def show
+        @item = Item.find(params[:id])
+        render json: @item, include: :category
+    end
+
+    def create
+        @item = Item.create(name: params[:name], image: params[:image], cart_id: params[:cart_id])
+        redirect_to 'http://localhost:3001/items'
+    end
+
+    def destroy 
+        @item = Item.find(params[:id])
+        @item.destroy
+
+        redirect_to "http://localhost:3001/items"
+     end 
+    
 end
