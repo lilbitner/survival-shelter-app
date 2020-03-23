7e01a1f17ebb6f5a7bb1f6e70856706368c6edef
brandong1 <brandon@brandonleeproductions.com>
2020-03-23 13:08:33 -0600

    create cart controller actions

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,2 +1,26 @@
 class CartsController < ApplicationController
+    def index 
+        @carts = Cart.all
+        render json: @carts, include: :category
+     end 
+ 
+     def show
+        @cart = Cart.find(params[:id])
+        render json: @cart, include: :category
+     end 
+     
+     def update
+        @cart = Cart.find(params[:id])
+        @cart.update(item: params[:item])
+        render json: @cart, status: :accepted 
+
+        redirect_to 'http://localhost:3001/checkout.html'
+     end
+
+     def destroy 
+        @cart = Cart.find(params[:id])
+        @cart.destroy 
+
+        render status: :no_content
+     end 
 end
