68df037d8762ebba1d8f8ece12efff95d3d52cc9
brandong1 <brandon@brandonleeproductions.com>
2020-03-23 11:23:24 -0600

    create database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+Item.destroy_all
+
+category1 = Category.create(name: "Food")
+category2 = Category.create(name: "Medical")
+category3 = Category.create(name: "Tools")
+category4 = Category.create(name: "Shelter")
+
+item1 = Item.create(name: "Rice", category: category1)
+item2 = Item.create(name: "Shotgun", category: category3)
+item3 = Item.create(name: "Bunker", category: category4)
+item4 = Item.create(name: "Oatmeal", category: category1)
+item5 = Item.create(name: "Knife", category: category3)
+item6 = Item.create(name: "Bandaid", category: category2)
+item7 = Item.create(name: "Gauze", category: category2)
+item8 = Item.create(name: "Latex gloves", category: category2)
+item9 = Item.create(name: "Home", category: category4)
+item10 = Item.create(name: "Alcohol", category: category2)
+item11 = Item.create(name: "Toilet Paper", category: category3)
+
+cart1= Cart.create()
