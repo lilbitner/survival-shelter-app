bce2274937f611e8ff373e482d6cb7ed796c9f67
brandong1 <brandon@brandonleeproductions.com>
2020-03-23 13:08:01 -0600

    create categories controller actions

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,11 @@
 class CategoriesController < ApplicationController
+    def index
+        @categories = Category.all
+        render json: @items, include: :category
+    end
+
+    def show
+        @category = Category.find(params[:id])
+        render json: @category, include: :category
+    end
 end
